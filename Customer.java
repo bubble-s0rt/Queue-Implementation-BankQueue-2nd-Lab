399bb6a93ac7dcd36ef77167698ea8624e5c3e9a
ID- 1904023, 1904024 <cvsmain01@gmail.com>
2024-09-06 14:23:24 +0600

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,11 +2,13 @@ public class Customer {
     private final int arrivalTime;
     private final int serviceTime;
     private boolean served;
+    private boolean leftQueue;
 
     public Customer(int arrivalTime, int serviceTime) {
         this.arrivalTime = arrivalTime;
         this.serviceTime = serviceTime;
-        this.served = true;
+        this.served = false;  // Initially, the customer is not served.
+        this.leftQueue = false;  // Initially, the customer hasn't left the queue.
     }
 
     public int getArrivalTime() {
@@ -23,5 +25,13 @@ public class Customer {
 
     public void setServed(boolean served) {
         this.served = served;
+    }
+
+    public boolean isLeftQueue() {
+        return leftQueue;
+    }
+
+    public void setLeftQueue(boolean leftQueue) {
+        this.leftQueue = leftQueue;
     }
 }
