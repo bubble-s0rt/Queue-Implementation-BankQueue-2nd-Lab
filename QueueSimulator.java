122a5ecbbe603d94725c4c0291bd7ca2dcae2aad
ID- 1904023, 1904024 <cvsmain01@gmail.com>
2024-09-06 14:27:20 +0600

    Update QueueSimulator.java

--- QueueSimulator.java
+++ QueueSimulator.java
@@ -6,17 +6,19 @@ import java.util.concurrent.TimeUnit;
 public class QueueSimulator {
     private final int simulationTime;
     private final BankQueue bankQueue;
+    private final GroceryQueues groceryQueues;
     private int totalCustomers;
     private int customersServed;
     private int customersLeft;
     private int totalServiceTime;
     private final Random random;
 
-    private final int scaleFactor = 10; // Scale factor for time
+    private final int scaleFactor = 10;  // For time scaling
 
-    public QueueSimulator(int simulationTime, int numberOfTellers, int maxQueueLength) {
+    public QueueSimulator(int simulationTime, int numberOfTellers, int maxQueueLength, int numberOfCashiers, int groceryMaxQueueLength) {
         this.simulationTime = simulationTime;
         this.bankQueue = new BankQueue(numberOfTellers, maxQueueLength);
+        this.groceryQueues = new GroceryQueues(numberOfCashiers, groceryMaxQueueLength);
         this.totalCustomers = 0;
         this.customersServed = 0;
         this.customersLeft = 0;
@@ -25,9 +27,9 @@ public class QueueSimulator {
     }
 
     public void startSimulation() {
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(bankQueue.getNumberOfTellers() + 1);
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(bankQueue.getNumberOfTellers() + groceryQueues.getNumberOfCashiers() + 2);
 
-        // Task for customer arrival
+        // Task for customer arrival for BankQueue
         executor.scheduleAtFixedRate(() -> {
             int arrivalTime = (int) (System.currentTimeMillis() / 1000);
             int serviceTime = random.nextInt(241) + 60;
@@ -39,10 +41,39 @@ public class QueueSimulator {
             }
         }, 0, (random.nextInt(41) + 20) * scaleFactor, TimeUnit.MILLISECONDS);
 
-        // Tasks for tellers to serve customers
+        // Task for customer arrival for GroceryQueues
+        executor.scheduleAtFixedRate(() -> {
+            int arrivalTime = (int) (System.currentTimeMillis() / 1000);
+            int serviceTime = random.nextInt(241) + 60;
+            Customer customer = new Customer(arrivalTime, serviceTime);
+            if (groceryQueues.addCustomer(customer)) {
+                totalCustomers++;
+            } else {
+                customersLeft++;
+            }
+        }, 0, (random.nextInt(41) + 20) * scaleFactor, TimeUnit.MILLISECONDS);
+
+        // Task for BankQueue tellers
         for (int i = 0; i < bankQueue.getNumberOfTellers(); i++) {
             executor.scheduleAtFixedRate(() -> {
                 Customer customer = bankQueue.getNextCustomer();
+                if (customer != null) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(customer.getServiceTime() * scaleFactor);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    totalServiceTime += customer.getServiceTime();
+                    customersServed++;
+                }
+            }, 0, 1, TimeUnit.MILLISECONDS);
+        }
+
+        // Task for GroceryQueues cashiers
+        for (int i = 0; i < groceryQueues.getNumberOfCashiers(); i++) {
+            int cashierIndex = i;
+            executor.scheduleAtFixedRate(() -> {
+                Customer customer = groceryQueues.getNextCustomer(cashierIndex);
                 if (customer != null) {
                     try {
                         TimeUnit.MILLISECONDS.sleep(customer.getServiceTime() * scaleFactor);
@@ -74,15 +105,17 @@ public class QueueSimulator {
         System.out.println("Total customers arrived: " + totalCustomers);
         System.out.println("Total customers served: " + customersServed);
         System.out.println("Total customers left without being served: " + customersLeft);
-        System.out.println("Average service time: " + (customersServed > 0 ? (totalServiceTime / customersServed) / (scaleFactor) : 0) + " minutes");
+        System.out.println("Average service time: " + (customersServed > 0 ? (totalServiceTime / customersServed) / scaleFactor : 0) + " minutes");
         System.out.println("Total simulation time: " + simulationTime + " minutes");
     }
 
     public static void main(String[] args) {
-        int simulationTime = 120; // Simulation time in minutes
+        int simulationTime = 120;  // Simulation time in minutes
         int numberOfTellers = 3;
         int maxQueueLength = 5;
-        QueueSimulator simulator = new QueueSimulator(simulationTime, numberOfTellers, maxQueueLength);
+        int numberOfCashiers = 3;
+        int groceryMaxQueueLength = 2;
+        QueueSimulator simulator = new QueueSimulator(simulationTime, numberOfTellers, maxQueueLength, numberOfCashiers, groceryMaxQueueLength);
         simulator.startSimulation();
     }
 }
